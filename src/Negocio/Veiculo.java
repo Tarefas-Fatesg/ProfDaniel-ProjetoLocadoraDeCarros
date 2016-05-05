b619a39c82b58ae61e272320c4ac1afa7ed408e7
yangoff <yangovip@gmail.com>
2016-05-05 17:52:12 -0300

    Inicio projeto

---
+++ src/Negocio/Veiculo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author kai
+ */
+public class Veiculo {
+    private long id;
+    private Modelo modelo;
+    private String placa;
+    private String chassi;
+    private String cor;
+    private Calendar ano;
+    private Acessorios acessorio[];
+    private Manutencao manutencao[];
+    
+}
